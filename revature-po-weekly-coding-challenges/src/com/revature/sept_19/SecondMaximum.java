0310736ccc96b6c7b8521f558fb576e5f1d97f9a
Shushmita Das <35471725+shushmitadas99@users.noreply.github.com>
2022-09-22 10:46:28 -0400

    sept-19-submission

---
+++ revature-po-weekly-coding-challenges/src/com/revature/sept_19/SecondMaximum.java
@@ -0,0 +1,29 @@
+package com.revature.sept_19;
+
+import java.util.Scanner;
+
+public class SecondMaximum {
+
+    public static void main(String[] args) {
+        int N=0, num1=0, num2=0, num3=0;
+        Scanner sc = new Scanner(System.in);
+
+        if(sc.hasNextInt()) {
+            N = sc.nextInt();
+        }
+
+        while(N --> 0){ // while condition N > 0, decrement N --
+            num1 = sc.nextInt();
+            num2 = sc.nextInt();
+            num3 = sc.nextInt();
+
+            if ((num1 > num2 && num1 < num3) || (num1 < num2 && num1 > num3)) {
+                System.out.println(num1);
+            }else if ((num2 > num1 && num2 < num3) || (num2 < num1 && num2 > num3)){
+                System.out.println(num2);
+            }else if ((num3 > num1 && num3 < num2) || (num3 < num1 && num3 > num2)){
+                System.out.println(num3);
+            }
+        }
+    }
+}
